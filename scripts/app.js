322c870ac30743b4f5fef8f27029e59d602c5d14
WANTZThomas <t.wantz93@gmail.com>
2018-11-05 17:47:07 +0100

    Add shooter setup and addTarget

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
+const shooter= {}
 
+/**
+ * Set up
+ */
+
+ shooter.$container = document.querySelector('.shooter')
+ shooter.$score = shooter.$container.querySelector('.start')
+ shooter.$start = shooter.$container.querySelector('.score .value')
+ shooter.$timer = shooter.$container.querySelector('.timer')
+ shooter.$targets = shooter.$container.querySelector('.targets')
+
+/**
+ * Shooter
+ */
+
+ shooter.addTarget = () => 
+ {
+     const $target = document.createElement('div')
+     $target.classList.add('target')
+     $target.style.top = `${Math.random() * 100}%`
+     $target.style.left = `${Math.random() * 100}%`
+     shooter.$targets.appendChild($target)
+ }
+
+ shooter.addTarget()
